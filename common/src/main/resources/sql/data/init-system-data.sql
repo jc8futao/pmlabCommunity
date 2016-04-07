dd1f0f9325660d055212906aa80b1e2ec3282c44
tfu <jc8futao@hotmail.com>
2016-04-07 17:55:09 +0800

    update database init sql

--- common/src/main/resources/sql/data/init-system-data.sql
+++ common/src/main/resources/sql/data/init-system-data.sql
@@ -1,18 +1,9 @@
-#如果复制到mysql中执行时 加上
-#DELIMITER ;;
+-- ----------------------------
+-- Records of sys_user
+-- ----------------------------
+INSERT INTO `sys_user` VALUES ('1', 'oYuQKs43fYz4QilcMH-to8jwEmkM', 'admin', 'admin', '', '', 'd3c59d25033dbf980d29554025c23a75', '8d78869f470951332959580424d4bf4f', '2016-04-07 17:40:49', '0', '', '0', '0', '', '1', '0', '系统内置超级管理员');
 
-truncate `sys_user`;;
-/*默认admin/123456*/
-insert into `sys_user`
-(`id`, `username`, `openid`,`email`, `mobilePhoneNumber`, `password`, `salt`, `createDate`, `admin`)
-  values
-  (1, 'admin', 'admin@pmlabs.cn', '','13412345671', 'd3c59d25033dbf980d29554025c23a75', '8d78869f470951332959580424d4bf4f', sysdate(), 1);;
-
-truncate table `sys_role`;;
-insert into `sys_role` (`id`, `name`, `role`, `description`, `is_show`) values (1,  '超级管理员', 'admin', '拥有所有权限', 1);;
-truncate table `sys_permission`;;
-insert into `sys_permission` (`id`, `name`, `permission`, `description`, `is_show`) values (1, '所有', '*', '所有数据操作的权限', 1);;
-
-truncate table `sys_auth`;;
-insert into sys_auth (`id`, `user_id`, `role_ids`)
-  values(1, 1, '1');;
+-- ----------------------------
+-- Records of sys_role
+-- ----------------------------
+INSERT INTO `sys_role` VALUES ('1', 'superadmin', 'superadmin', '超级管理员', '0', '0');
--- common/src/main/resources/sql/schema/init-system-schema.sql
+++ common/src/main/resources/sql/schema/init-system-schema.sql
@@ -1,71 +1,142 @@
-#如果复制到mysql中执行时 加上
-#DELIMITER ;;
+SET FOREIGN_KEY_CHECKS=0;
 
-drop table if exists `sys_user`;;
-drop table if exists `sys_role`;;
-drop table if exists `sys_permission`;;
-drop table if exists `sys_role_resource_permission`;;
-drop table if exists `sys_auth`;;
+-- ----------------------------
+-- Table structure for pmlab_enroll
+-- ----------------------------
+DROP TABLE IF EXISTS `pmlab_enroll`;
+CREATE TABLE `pmlab_enroll` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `eventId` bigint(20) NOT NULL,
+  `userId` bigint(20) NOT NULL,
+  `realName` varchar(64) NOT NULL,
+  `mobilePhoneNumber` varchar(32) NOT NULL,
+  `company` varchar(64) NOT NULL DEFAULT '',
+  `jobTitle` varchar(32) NOT NULL DEFAULT '',
+  `enrollTime` datetime NOT NULL,
+  `enrollStatus` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_eventId` (`eventId`),
+  KEY `index_userId` (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-##user
-create table `sys_user`(
-  `id`         bigint not null auto_increment,
-  `openid`  varchar(100),
-  `username`  varchar(100),
-  `email`  varchar(100),
-  `mobilePhoneNumber`  varchar(20),
-  `password`  varchar(100),
-  `salt`       varchar(100),
-  `createDate` timestamp default 0,
-  `admin`     bool,
-  constraint `pk_sys_user` primary key(`id`),
-  constraint `unique_sys_user_username` unique(`username`),
-  constraint `unique_sys_user_email` unique(`email`),
-  constraint `unique_sys_user_mobile_phone_number` unique(`mobilePhoneNumber`)
-) charset=utf8 ENGINE=InnoDB;;
-alter table `sys_user` auto_increment=1000;;
+-- ----------------------------
+-- Records of pmlab_enroll
+-- ----------------------------
 
-create table `sys_permission`(
-  `id`         bigint not null auto_increment,
-  `name`      varchar(100),
-  `permission`  varchar(100),
-  `description`      varchar(200),
-  `show`       bool,
-  constraint `pk_sys_permission` primary key(`id`),
-  index idx_sys_permission_name (`name`),
-  index idx_sys_permission_permission (`permission`),
-  index idx_sys_permission_show (`show`)
-) charset=utf8 ENGINE=InnoDB;;
-alter table `sys_permission` auto_increment=1000;;
+-- ----------------------------
+-- Table structure for pmlab_event
+-- ----------------------------
+DROP TABLE IF EXISTS `pmlab_event`;
+CREATE TABLE `pmlab_event` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `creatorId` bigint(20) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '0',
+  `title` varchar(512) NOT NULL,
+  `coverUrl` varchar(512) NOT NULL DEFAULT '',
+  `content` text NOT NULL,
+  `limited` int(11) NOT NULL DEFAULT '0',
+  `enrollNum` int(11) NOT NULL DEFAULT '0',
+  `target` int(11) NOT NULL DEFAULT '0',
+  `address` varchar(512) NOT NULL DEFAULT '',
+  `published` tinyint(1) NOT NULL DEFAULT '0',
+  `publishTime` datetime DEFAULT NULL,
+  `closingTime` datetime NOT NULL,
+  `startTime` datetime NOT NULL,
+  `finishDate` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create table `sys_role`(
-  `id`         bigint not null auto_increment,
-  `name`      varchar(100),
-  `role`  varchar(100),
-  `description`      varchar(200),
-  `show`       bool,
-  constraint `pk_sys_role` primary key(`id`),
-  index `idx_sys_role_name` (`name`),
-  index `idx_sys_role_role` (`role`),
-  index `idx_sys_role_show` (`show`)
-) charset=utf8 ENGINE=InnoDB;;
-alter table `sys_role` auto_increment=1000;;
+-- ----------------------------
+-- Records of pmlab_event
+-- ----------------------------
 
-create table `sys_role_resource_permission`(
-  `id`         bigint not null auto_increment,
-  `role_id`   bigint,
-  `resource_id` bigint,
-  `permission_ids` varchar(500),
-  constraint `pk_sys_role_resource_permission` primary key(`id`),
-  constraint `unique_sys_role_resource_permission` unique(`role_id`, `resource_id`)
-) charset=utf8 ENGINE=InnoDB;;
-alter table `sys_role_resource_permission` auto_increment=1000;;
+-- ----------------------------
+-- Table structure for sys_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_permission`;
+CREATE TABLE `sys_permission` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `role` varchar(64) NOT NULL,
+  `permission` varchar(64) NOT NULL,
+  `description` varchar(512) NOT NULL DEFAULT '',
+  `show` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create table `sys_auth`(
-  `id`         bigint not null auto_increment,
-  `user_id`        bigint,
-  `role_ids`       varchar(500),
-  constraint `pk_sys_auth` primary key(`id`),
-  index `idx_sys_auth_user` (`user_id`)
-) charset=utf8 ENGINE=InnoDB;;
-alter table `sys_auth` auto_increment=1000;;
+-- ----------------------------
+-- Records of sys_permission
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `role` varchar(64) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `description` varchar(512) NOT NULL DEFAULT '',
+  `buildIn` tinyint(1) NOT NULL DEFAULT '0',
+  `show` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `index_role` (`role`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for sys_role_permission_relation
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role_permission_relation`;
+CREATE TABLE `sys_role_permission_relation` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `roleId` bigint(20) NOT NULL,
+  `permissionId` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_role_permission_relation
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `openid` varchar(64) NOT NULL DEFAULT '',
+  `username` varchar(64) NOT NULL,
+  `realName` varchar(64) NOT NULL,
+  `mobilePhoneNumber` varchar(32) NOT NULL,
+  `email` varchar(128) NOT NULL DEFAULT '',
+  `password` varchar(64) NOT NULL,
+  `salt` varchar(64) NOT NULL,
+  `createDate` datetime NOT NULL,
+  `createSources` tinyint(4) NOT NULL,
+  `companyName` varchar(256) NOT NULL DEFAULT '',
+  `jobTitle` varchar(64) NOT NULL,
+  `jobType` tinyint(4) NOT NULL,
+  `jobDescription` varchar(512) NOT NULL DEFAULT '',
+  `admin` tinyint(1) NOT NULL DEFAULT '0',
+  `locked` tinyint(1) NOT NULL DEFAULT '0',
+  `description` varchar(512) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index_openId` (`openid`),
+  UNIQUE KEY `index_mobile` (`mobilePhoneNumber`),
+  KEY `index_credential` (`username`,`password`,`salt`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for sys_user_role_relation
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user_role_relation`;
+CREATE TABLE `sys_user_role_relation` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `userId` bigint(20) NOT NULL,
+  `roleId` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_userId` (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_user_role_relation
+-- ----------------------------
