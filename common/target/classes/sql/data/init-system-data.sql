d9e325a910345a327e250a3f88ce4fea64fd8cea
mazip <314059610@qq.com>
2016-04-11 21:09:06 +0800

    update ignore

--- common/target/classes/sql/data/init-system-data.sql
+++
@@ -1,9 +0,0 @@
--- ----------------------------
--- Records of sys_user
--- ----------------------------
-INSERT INTO `sys_user` VALUES ('1', 'oYuQKs43fYz4QilcMH-to8jwEmkM', 'admin', 'admin', '', '', 'd3c59d25033dbf980d29554025c23a75', '8d78869f470951332959580424d4bf4f', '2016-04-07 17:40:49', '0', '', '0', '0', '', '1', '0', '系统内置超级管理员');
-
--- ----------------------------
--- Records of sys_role
--- ----------------------------
-INSERT INTO `sys_role` VALUES ('1', 'superadmin', 'superadmin', '超级管理员', '0', '0');
